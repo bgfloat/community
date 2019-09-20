cc590475c869ff07e5a8c2a73bc62b1130a45d6b
sun <1243055060@qq.com>
2019-09-20 23:06:20 +0800

    interceptor

--- src/main/java/life/zhuyuan/community/controller/IndexController.java
+++ src/main/java/life/zhuyuan/community/controller/IndexController.java
@@ -1,8 +1,6 @@
 package life.zhuyuan.community.controller;
 
 import life.zhuyuan.community.dto.PaginationDTO;
-import life.zhuyuan.community.mapper.UserMapper;
-import life.zhuyuan.community.model.User;
 import life.zhuyuan.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,37 +8,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
 public class IndexController {
 
     @Autowired
-    private UserMapper userMapper;
-    @Autowired
     private QuestionService questionService;
 
     @GetMapping("/")
-    public String index(HttpServletRequest request,
-                        Model model,
+    public String index( Model model,
                         @RequestParam(name = "page", defaultValue = "1") Integer page,
                         @RequestParam(name = "size", defaultValue = "5") Integer size
     ) {
-        Cookie[] cookies = request.getCookies();
-        if (null != cookies && cookies.length != 0) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
-
         PaginationDTO pagination = questionService.list(page, size);
         model.addAttribute("pagination", pagination);
         return "index";
--- src/main/java/life/zhuyuan/community/controller/ProfileController.java
+++ src/main/java/life/zhuyuan/community/controller/ProfileController.java
@@ -1,7 +1,6 @@
 package life.zhuyuan.community.controller;
 
 import life.zhuyuan.community.dto.PaginationDTO;
-import life.zhuyuan.community.mapper.UserMapper;
 import life.zhuyuan.community.model.User;
 import life.zhuyuan.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,14 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ProfileController {
 
-    @Autowired
-    private UserMapper userMapper;
     @Autowired
     private QuestionService questionService;
 
@@ -29,29 +25,18 @@ public class ProfileController {
                           @RequestParam(name = "page", defaultValue = "1") Integer page,
                           @RequestParam(name = "size", defaultValue = "5") Integer size
     ) {
+        //判断session中有没有user
+        User user = (User) request.getSession().getAttribute("user");
+        if (null == user) {
+            return "redirect:/";
+        }
+
         if ("questions".equals(action)) {
             model.addAttribute("section", "questions");
             model.addAttribute("sectionName", "我的提问");
         } else if ("replies".equals(action)) {
             model.addAttribute("section", "replies");
             model.addAttribute("sectionName", "最新回复");
-        }
-        User user = null;
-        Cookie[] cookies = request.getCookies();
-        if (null != cookies && cookies.length != 0) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
-        if (null == user) {
-            return "redirect:/";
         }
         PaginationDTO paginationDTO = questionService.list(user.getId(), page, size);
         model.addAttribute("pagination", paginationDTO);
