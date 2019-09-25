02049b29af5ed3def10c9cb9fc3aa468be01440b
sun <1243055060@qq.com>
2019-09-25 23:35:26 +0800

    comment interface

---
+++ src/main/java/life/zhuyuan/community/controller/CommentController.java
@@ -0,0 +1,30 @@
+package life.zhuyuan.community.controller;
+
+import life.zhuyuan.community.dto.CommentDTO;
+import life.zhuyuan.community.mapper.CommentMapper;
+import life.zhuyuan.community.model.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class CommentController {
+
+    @Autowired
+    private CommentMapper commentMapper;
+
+    @RequestMapping(value = "/comment", method = RequestMethod.POST)
+    public Object post(@RequestBody CommentDTO commentDTO) {
+        Comment comment = new Comment();
+        comment.setParentId(commentDTO.getParentId());
+        comment.setContent(commentDTO.getContent());
+        comment.setType(commentDTO.getType());
+        comment.setGmtCreate(System.currentTimeMillis());
+        comment.setGmtModified(System.currentTimeMillis());
+        comment.setCommentator(1);
+        commentMapper.insert(comment);
+        return null;
+    }
+}
