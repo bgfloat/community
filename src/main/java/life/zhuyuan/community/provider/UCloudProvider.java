630c86fdb01ea2319f3b4f0f7e87a34bde91e81f
sun <1243055060@qq.com>
2019-10-11 23:18:15 +0800

    使用SDK上传图片

---
+++ src/main/java/life/zhuyuan/community/provider/UCloudProvider.java
@@ -0,0 +1,79 @@
+package life.zhuyuan.community.provider;
+
+import cn.ucloud.ufile.UfileClient;
+import cn.ucloud.ufile.api.object.ObjectConfig;
+import cn.ucloud.ufile.auth.ObjectAuthorization;
+import cn.ucloud.ufile.auth.UfileObjectLocalAuthorization;
+import cn.ucloud.ufile.bean.PutObjectResultBean;
+import cn.ucloud.ufile.exception.UfileClientException;
+import cn.ucloud.ufile.exception.UfileServerException;
+import life.zhuyuan.community.exception.CustomizeErrorCode;
+import life.zhuyuan.community.exception.CustomizeException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.UUID;
+
+@Service
+public class UCloudProvider {
+
+    @Value("${ucloud.ufile.public-key}")
+    private String publicKey;
+    @Value("${ucloud.ufile.private-key}")
+    private String privateKey;
+    private String bucketName = "zhuyuan";
+
+
+    public String upload(InputStream fileStream, String mimeType, String fileName) {
+        String generatedFileName;
+        String[] filePaths = fileName.split("\\.");
+        if (filePaths.length > 1) {
+            generatedFileName = UUID.randomUUID().toString() + "." + filePaths[filePaths.length - 1];
+        } else {
+            throw new CustomizeException(CustomizeErrorCode.FILE_UPLOAD_FAIL);
+        }
+
+        try {
+            // 对象相关API的授权器
+            ObjectAuthorization objectAuthorization = new UfileObjectLocalAuthorization(publicKey, privateKey);
+            // 对象操作需要ObjectConfig来配置您的地区和域名后缀
+            ObjectConfig config = new ObjectConfig("cn-bj", "ufileos.com");
+            PutObjectResultBean response = UfileClient.object(objectAuthorization, config)
+                    .putObject(fileStream, mimeType)
+                    .nameAs(generatedFileName)
+                    .toBucket(bucketName)
+                    /**
+                     * 是否上传校验MD5, Default = true
+                     */
+                    //  .withVerifyMd5(false)
+                    /**
+                     * 指定progress callback的间隔, Default = 每秒回调
+                     */
+                    //  .withProgressConfig(ProgressConfig.callbackWithPercent(10))
+                    /**
+                     * 配置进度监听
+                     */
+                    .setOnProgressListener((bytesWritten, contentLength) -> {
+                    })
+                    .execute();
+            //分析响应
+            if (response != null || response.getRetCode() == 0) {
+                String url = UfileClient.object(objectAuthorization, config)
+                        .getDownloadUrlFromPrivateBucket(generatedFileName, bucketName, 24 * 60 * 60)
+                        .createUrl();
+                return url;
+            } else {
+                throw new CustomizeException(CustomizeErrorCode.FILE_UPLOAD_FAIL);
+            }
+        } catch (UfileClientException e) {
+            e.printStackTrace();
+            throw new CustomizeException(CustomizeErrorCode.FILE_UPLOAD_FAIL);
+        } catch (UfileServerException e) {
+            e.printStackTrace();
+            throw new CustomizeException(CustomizeErrorCode.FILE_UPLOAD_FAIL);
+
+        }
+    }
+}
